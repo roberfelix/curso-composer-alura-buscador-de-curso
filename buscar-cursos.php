92a474669ecb2116b6c4a8ba7043486b107d1bd6
Robervaldo Felix <robervaldo.felix@hotmail.com>
2022-01-16 21:02:04 -0300

    Adicionando arquivo bin

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 require 'vendor/autoload.php';
@@ -6,12 +7,12 @@ use Rober\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
-$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$client = new Client(['base_uri' => 'https://www.alura.com.br/', 'verify' => false]);
 $crawler = new Crawler();
 
 $buscardor = new Buscador($client, $crawler);
 $cursos = $buscardor->buscar('/cursos-online-programacao/php');
 
 foreach ($cursos as $curso) {
-    echo exibeMensagem($curso);
+    exibeMensagem($curso);
 }
